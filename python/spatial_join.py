fb4104e2fd027b05f58fe9eb8335dd581210cd2b
zhangjianting <zjtnssl@yahoo.com>
2020-11-17 00:34:49 -0500

    add timing

--- python/spatial_join.py
+++ python/spatial_join.py
@@ -46,27 +46,34 @@ num_levels = 15
 min_size = 512
 scale = max(abs(x2 - x1), abs(y2 - y1)) / ((1 << num_levels) - 2);
 
-
+start = time.time()
 key_to_point,quadtree = cuspatial.quadtree_on_points(points_df['x'],points_df['y'],
     x1,x2,y1,y2, scale,num_levels, min_size)
-print(len(quadtree))
+end = time.time()
 
+print('number of nodes:',len(quadtree))
 print('node count:', (quadtree['is_quad'] == True).sum())
 print('leaf count:', (quadtree['is_quad'] == False).sum())
+print('quadtree construction time :', (end-start)*1000)
 
 poly_bboxes = cuspatial.polygon_bounding_boxes(
     ply_fpos, ply_rpos, ply_vertices['x'], ply_vertices['y'])
 print(len(poly_bboxes))    
 
+start = time.time()
 intersections = cuspatial.join_quadtree_and_bounding_boxes(
     quadtree, poly_bboxes,x1,x2,y1,y2, scale, num_levels,
 )
-print(len(intersections))    
+end = time.time()
+print('number of polygon-quadrant pairs:',len(intersections))    
+print('spatial filtering time :', (end-start)*1000)
 
+start = time.time()
 polygons_and_points = cuspatial.quadtree_point_in_polygon(
     intersections, quadtree,key_to_point,
     points_df['x'],points_df['y'],
     ply_fpos,ply_rpos,ply_vertices['x'],ply_vertices['y'])
+end = time.time()
 print(len(polygons_and_points)) 
-
+print('spatial refinement time :', (end-start)*1000)
 
