d6019c28465396b1a21dcd388b05f571aead093b
zhangjianting <zjtnssl@yahoo.com>
2020-11-17 00:30:06 -0500

    add data sources

--- python/gen_quadtree.py
+++ python/gen_quadtree.py
@@ -4,6 +4,19 @@ import cudf
 import cuspatial
 import numpy as np
  
+# all .cny files can be downloaded from http://geoteci.engr.ccny.cuny.edu/nyctaxidata/
+# the binary files are int32 type in the unit of feet using projection epsg 2236 (for NYC and Long Island)
+# each record has 4 fields (pick_up_lon, pick_up_lat,drop_off_lon, drop_off_lat), only the first 2 fields are used
+# the orginal 2009 yellow cab data can be accessed from https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page 
+
+# The taxi zone data in ESRI shapefile format can be accessed from https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+# which is linked to https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip; the shapefile also uses EPSG 2263 projection  
+
+# The NYC Community District Data (nycd) and 2000 Census Tract data (nyct2000) can be downloaded from 
+# https://www1.nyc.gov/site/planning/data-maps/open-data.page#district_political
+# version 11a released in 2011 were used; both use EPSG 2263 projection 
+
+
 cuspatial_data_path='/home/microway/cuspatial_data' 
 polygon_shapefile_name='taxi_zones.shp'
 #polygon_shapefile_name='nycd_11a_av/nycd.shp'
--- python/spatial_join.py
+++ python/spatial_join.py
@@ -3,7 +3,19 @@ import cupy
 import cudf
 import cuspatial
 import numpy as np
- 
+
+# all .cny files can be downloaded from http://geoteci.engr.ccny.cuny.edu/nyctaxidata/
+# the binary files are int32 type in the unit of feet using projection epsg 2236 (for NYC and Long Island)
+# each record has 4 fields (pick_up_lon, pick_up_lat,drop_off_lon, drop_off_lat), only the first 2 fields are used
+# the orginal 2009 yellow cab data can be accessed from https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page 
+
+# The taxi zone data in ESRI shapefile format can be accessed from https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+# which is linked to https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip; the shapefile also uses EPSG 2263 projection  
+
+# The NYC Community District Data (nycd) and 2000 Census Tract data (nyct2000) can be downloaded from 
+# https://www1.nyc.gov/site/planning/data-maps/open-data.page#district_political
+# version 11a released in 2011 were used; both use EPSG 2263 projection 
+
 cuspatial_data_path='/home/microway/cuspatial_data' 
 polygon_shapefile_name='taxi_zones.shp'
 #polygon_shapefile_name='nycd_11a_av/nycd.shp'
@@ -34,6 +46,7 @@ num_levels = 15
 min_size = 512
 scale = max(abs(x2 - x1), abs(y2 - y1)) / ((1 << num_levels) - 2);
 
+
 key_to_point,quadtree = cuspatial.quadtree_on_points(points_df['x'],points_df['y'],
     x1,x2,y1,y2, scale,num_levels, min_size)
 print(len(quadtree))
@@ -55,4 +68,5 @@ polygons_and_points = cuspatial.quadtree_point_in_polygon(
     points_df['x'],points_df['y'],
     ply_fpos,ply_rpos,ply_vertices['x'],ply_vertices['y'])
 print(len(polygons_and_points)) 
+
 
