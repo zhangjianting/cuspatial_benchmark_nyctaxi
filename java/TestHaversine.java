ea723440b3f42e6567d31dd6a8a4ab34bc1fceb2
zhangjianting <zjtnssl@yahoo.com>
2020-11-22 20:33:46 -0500

    Update TestHaversine.java

--- java/TestHaversine.java
+++ java/TestHaversine.java
@@ -1,7 +1,5 @@
 import ai.rapids.cudf.*;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import ai.rapids.cuspatial.*;
 
 import java.util.Arrays;
 import java.util.function.Supplier;
@@ -11,6 +9,12 @@ public class TestHaversine
 {
     public static void main(String[] args)
     {
-
+      ColumnVector aLon = ColumnVector.fromDoubles(-180, 180);
+      ColumnVector aLat = ColumnVector.fromDoubles(0, 30);
+      ColumnVector bLon = ColumnVector.fromDoubles(180, -180);
+      ColumnVector bLat = ColumnVector.fromDoubles(0, 30);
+      ColumnVector result = CuSpatial.haversineDistance(aLon, aLat, bLon, bLat);
+      HostColumnVector h_dis=result.copyToHost();
+      System.println(h_dis.toString());
     }
 }
