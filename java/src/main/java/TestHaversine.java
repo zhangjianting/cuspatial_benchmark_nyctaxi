5146e0fd5c7a20def9b810ff9a560c0139337648
zhangjianting <zjtnssl@yahoo.com>
2020-11-23 16:37:46 -0500

    add haversine tests for java/python using taxi trip data

--- java/src/main/java/TestHaversine.java
+++ java/src/main/java/TestHaversine.java
@@ -5,10 +5,25 @@ public class TestHaversine
 {
     public static void main(String[] args)
     {
-      ColumnVector aLon = ColumnVector.fromDoubles(-180, 180);
+      /*ColumnVector aLon = ColumnVector.fromDoubles(-180, 180);
       ColumnVector aLat = ColumnVector.fromDoubles(0, 30);
       ColumnVector bLon = ColumnVector.fromDoubles(180, -180);
-      ColumnVector bLat = ColumnVector.fromDoubles(0, 30);
+      ColumnVector bLat = ColumnVector.fromDoubles(0, 30);*/
+
+      //all parameters are place holders
+      String csv_fn="";
+      Table table = Table.readCSV(schema, new File(csv_fn));
+      System.out.println(table.toString());
+
+      //"Start_Lon"
+      ColumnVector aLon=table.getColumn(5);
+      //"Start_Lat"
+      ColumnVector aLat=table.getColumn(6);
+      //"End_Lon"
+      ColumnVector bLon=table.getColumn(7);
+      //"End_Lat"
+      ColumnVector bLat=table.getColumn(8);
+
       ColumnVector result = CuSpatial.haversineDistance(aLon, aLat, bLon, bLat);
       HostColumnVector h_dis=result.copyToHost();
       System.out.println(h_dis.toString());
