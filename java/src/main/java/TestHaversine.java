ec50a020e61a62e268daf65efb8d270d82a1580b
zhangjianting <zjtnssl@yahoo.com>
2020-11-24 11:35:44 -0500

    using pickup/drop-off locations for Haversine distance in python/java

--- java/src/main/java/TestHaversine.java
+++ java/src/main/java/TestHaversine.java
@@ -11,8 +11,14 @@ public class TestHaversine
       ColumnVector bLon = ColumnVector.fromDoubles(180, -180);
       ColumnVector bLat = ColumnVector.fromDoubles(0, 30);*/
 
+      /*['vendor_name', 'Trip_Pickup_DateTime', 'Trip_Dropoff_DateTime',
+       'Passenger_Count', 'Trip_Distance', 'Start_Lon', 'Start_Lat',
+       'Rate_Code', 'store_and_forward', 'End_Lon', 'End_Lat', 'Payment_Type',
+       'Fare_Amt',
+      */
+
       //all parameters are place holders
-      String csv_fn="";
+      /*String csv_fn="";
       Schema schema = Schema.builder()
         .column(DType.INT32, "A")
         .column(DType.FLOAT64, "B")
@@ -20,16 +26,19 @@ public class TestHaversine
         .column(DType.STRING, "D")
         .build();
       Table table = Table.readCSV(schema, new File(csv_fn));
-      System.out.println(table.toString());
+      System.out.println(table.toString());*/
+
+      String parquet_fn="/home/jianting/cuspatial_data/200901.parquet";
+      Table table = Table.readCSV(new File(parquet_fn));
 
       //"Start_Lon"
-      ColumnVector aLon=table.getColumn(5);
+      ColumnVector aLon=table.getColumn(0);
       //"Start_Lat"
-      ColumnVector aLat=table.getColumn(6);
+      ColumnVector aLat=table.getColumn(1);
       //"End_Lon"
-      ColumnVector bLon=table.getColumn(7);
+      ColumnVector bLon=table.getColumn(2);
       //"End_Lat"
-      ColumnVector bLat=table.getColumn(8);
+      ColumnVector bLat=table.getColumn(3);
 
       ColumnVector result = CuSpatial.haversineDistance(aLon, aLat, bLon, bLat);
       HostColumnVector h_dis=result.copyToHost();
--- python/test_haversine.py
+++ python/test_haversine.py
@@ -11,7 +11,12 @@ start = time.time()
 df = read_csv("/home/jianting/cuspatial_data/yellow_tripdata_2009-01.csv")
 end = time.time()
 print("data ingesting time (from SSD) in ms={}".format((end - start) * 1000))
-df.head().to_pandas().columns
+
+print(df.head().to_pandas().columns)
+
+#save a suset of four columns for tests using java and python on jetson
+subset=df[['Start_Lon','Start_Lat','End_Lon','End_Lat']]
+subset.to_parquet("/home/jianting/cuspatial_data/200901.parquet")
 
 start = time.time()
 x1 = Series(df["Start_Lon"])
@@ -24,7 +29,6 @@ print(
         (end - start) * 1000
     )
 )
-
 start = time.time()
 h_dist = cuspatial.haversine_distance(x1, y1, x2, y2)
 end = time.time()
